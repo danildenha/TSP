7bd27776ccc268e7bd86b8c9347aba317febe0c8
danildenha <danildenha@gmail.com>
2024-04-03 09:39:24 -0500

    reading file

--- Driver.cpp
+++ Driver.cpp
@@ -12,14 +12,18 @@
 using namespace std;
 
 int main() {
-    Graph graph;
+    ifstream graph1("graph1.txt");
+    if (!graph1.is_open()) {
+        cout << "Couldn't open graph file" << endl;
+        return 1;
+    }
+    Graph* g = new Graph();
+    g->read(graph1);
+    g->print();
 
 
-
-
-
-
-
+    delete g;
+    graph1.close();
 
     return 0;
 }
